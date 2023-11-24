cfdec001a96a058cbb34aacc4299e313f32a2221
forteleaf <forteleaf@hotmail.com>
2023-11-24 13:37:16 +0900

    complete high score

--- high-score/lib/high_score.ex
+++ high-score/lib/high_score.ex
@@ -1,32 +1,36 @@
 defmodule HighScore do
+  @default_score 0
+
   def new() do
     # Please implement the new/0 function
     %{}
   end
 
-  def add_player(scores, name) do
+  def add_player(scores, name, score \\ @default_score) do
     # Please implement the add_player/3 function
-    %{:scores => score, :name => name}
-  end
-
-  def add_player(scores, name, score \\ 0) do
-    # Please implement the add_player/3 function
-    %{:scores => score, :name => name, :score => score}
+    Map.put(scores, name, score)
   end
 
   def remove_player(scores, name) do
     # Please implement the remove_player/2 function
+    Map.delete(scores, name)
   end
 
   def reset_score(scores, name) do
     # Please implement the reset_score/2 function
+    #    remove_player(scores, name)
+    #    add_player(scores, name)
+    Map.put(scores, name, @default_score)
   end
 
   def update_score(scores, name, score) do
     # Please implement the update_score/3 function
+    # 이 부분이 조금 난해
+    Map.update(scores, name, score, &(&1 + score))
   end
 
   def get_players(scores) do
     # Please implement the get_players/1 function
+    Map.keys(scores)
   end
 end
